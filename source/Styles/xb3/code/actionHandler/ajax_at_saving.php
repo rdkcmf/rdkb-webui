0a23f190fb18d6c7f3607e4ec94997e893456977
Pavan Kumar <PavanKumarReddy_B@infosys.com>
2016-02-29 18:06:35 +0530

    RDKB-2702: WiFi Spectrum Analyzer GUI
    Reason for change: WiFi Spectrum Analyzer GUI Develop Code
    Test Procedure: Test from GUI
    Risks: None
    Signed-off-by: Pavan Kumar PavanKumarReddy_B@infosys.com
    
    Change-Id: I435ed10aa043c233303fb1c7f7f47d794e12700e

---
+++ source/Styles/xb3/code/actionHandler/ajax_at_saving.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ If not stated otherwise in this file or this component's Licenses.txt file the
+ following copyright and licenses apply:
+ Copyright 2016 RDK Management
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+?>
+<?php
+$myfile = fopen("/var/tmp/Wifi_Spectrum_Analyzer_Table.html", "w");
+fwrite($myfile, "<style>table th tr {}</style>");
+fwrite($myfile, "<style>");
+fwrite($myfile, "h2 { background: #39baf1; color: #fff; padding: 10px; font-size: 1.1em; font-weight: bold; margin-bottom: 0; }");
+fwrite($myfile, "table { border-collapse: collapse; clear: both; width: 100%; background-color: #ededed; }");
+fwrite($myfile, "th { background: #39baf1; color: #fff; }");
+fwrite($myfile, "td { border: 1px solid white; }");
+fwrite($myfile, "</style>");
+fwrite($myfile, $_REQUEST['configInfo']);
+fclose($myfile);
+echo "success";
+?>
---
+++ source/Styles/xb3/code/wifi_spectrum_analyzer.php
@@ -0,0 +1,140 @@
+<?php include('includes/header.php'); ?>
+<div id="sub-header">
+    <?php include('includes/userbar.php'); ?>
+</div><!-- end #sub-header -->
+<?php include('includes/nav.php'); ?>
+<style>
+ td {
+    border: 1px solid white;
+}
+</style>
+<script>
+$(document).ready(function(){
+	comcast.page.init("Troubleshooting > Wi-Fi Spectrum Analyzer", "nav-wifi-spectrum-analyzer");
+	$("#save_result").hide();
+	function spanTable($rows, startIdx, endIdx){
+		if (endIdx === 0){
+			return;
+		}
+		var i , currentIndex = startIdx, count=1, lst=[];
+		var tds = $rows.find('td:eq('+ currentIndex +')');
+		var ctrl = $(tds[0]);
+		lst.push($rows[0]);
+		for (i=1;i<=tds.length;i++){
+			if (ctrl.text() ==  $(tds[i]).text()){
+				count++;
+				$(tds[i]).addClass('deleted');
+				lst.push($rows[i]);
+			}
+			else{
+				if (count>1){
+					ctrl.attr('rowspan',count);
+					spanTable($(lst),startIdx+1,endIdx-1)
+				}
+				count=1;
+				lst = [];
+				ctrl=$(tds[i]);
+				lst.push($rows[i]);
+			}
+		}
+	}
+	function ajax_spec_analyzer()
+	{
+		$("#save_result").hide();
+		$("#scan_status").html("Scanning...").show();
+		$("#start_scan").hide();
+		$.ajax({
+			type: "POST",
+			url: "actionHandler/ajax_wifi_spectrum_analyser.php",
+			success: function(result) {
+				result = JSON.parse(result);
+				if(result.status == "success")
+				{
+					$("#scan_status").hide();
+					var wifi_spec_values = result["data"];
+					$('#spec_capture_table tbody').remove('tr');
+					var table = $('#spec_capture_table tbody');
+					for (var index in wifi_spec_values)
+					{
+						var spec_values = wifi_spec_values[index];
+						var tr = $('<tr/>').appendTo(table);
+						var Radio_val = (spec_values['Radio'] == 'Device.WiFi.Radio.1') ? "2.4GHz": "5GHz";
+				        tr.append("<td headers='band_id' class='band_id'>" + Radio_val+ "</td>");
+				        tr.append("<td headers='channel_number' class='channel_number'>" + spec_values["Channel"]+ "</td>");
+				        tr.append("<td headers='mac_id' class='mac_id'>" + spec_values["BSSID"]+ "</td>");
+				        tr.append("<td headers='ssid_name' class='ssid_name'>" + spec_values["SSID"]+ "</td>");
+				        tr.append("<td headers='Signal_level' class='Signal_level'>" + spec_values["SignalStrength"]+ "</td>");
+				        tr.append("<td headers='mode' class='mode'>" + spec_values["SecurityModeEnabled"]+ "</td>");
+				        tr.append("<td headers='security' class='security'>" + spec_values["SupportedStandards"]+ "</td>");
+				        tr.append("<td headers='max_rate' class='max_rate'>" + spec_values["SupportedDataTransferRates"]+ "</td>");
+					}
+					spanTable($('#spec_capture_table tr:has(td)'),0,2);
+					$('#spec_capture_table .deleted').remove();
+					$("#start_scan").show();
+					$("#save_result").show();
+				}
+				else
+				{
+					setTimeout(function(){
+						ajax_spec_analyzer();
+					}, 5000);
+				}
+			},
+			failure: function()
+			{
+				//failure: function(){}
+			}
+		});
+	}
+	function popUp(URL) {
+	day = new Date();
+	id = day.getTime();
+	eval("page" + id + " = window.open(URL, '" + id + "', 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=700,height=400,left = 320.5,top = 105');");
+	}
+	$("#start_scan").click(function() {
+		ajax_spec_analyzer();
+	});
+	$("#save_result").click(function(){
+		table_data = $("#table_WSA").html();
+		jProgress('This may take several seconds', 60);
+		$.ajax({
+			type: "POST",
+			url: "actionHandler/ajax_at_saving.php",
+			data: { configInfo: table_data },
+			success: function(result) {
+				if(result == "success")
+				{
+					popUp("at_saving.php");
+					jHide();
+				}
+			}
+		});
+	});
+});
+</script>
+<div id="content" class="main_content">
+<h1>Troubleshooting > Wi-Fi Spectrum Analyzer</h1>
+	<div class="module">
+		<div>
+			<input type="button" class="btn" value="Start Scan" id = "start_scan"/>
+			<span id="scan_status"></span>
+			<input type="button" class="btn button" value="Save Result" id = "save_result" style="top: 8px;"/>
+		</div>
+	</div>
+	<div id="table_WSA" class="module data">
+		<h2>Wi-Fi Spectrum Analyzer Data</h2>
+		<table class="data" summary="Wi-Fi Spectrum Analyzer" id="spec_capture_table">
+			<tr>
+				<th id="band_id">Band</th>
+				<th id="channel_number" >Channel</th>
+				<th id="mac_id">MAC</th>
+				<th id="ssid_name">SSID</th>
+				<th id="Signal_level">SignalLevel</th>
+				<th id="mode">Mode</th>
+				<th id="security">Security</th>
+				<th id="max_rate">MaxRate</th>
+			</tr>
+		</table>
+	</div>
+</div>
+<?php include('includes/footer.php'); ?>
