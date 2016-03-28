74392088bffedf43c541eaacf5550e5de4be5286
Pavan Kumar <PavanKumarReddy_B@infosys.com>
2016-03-28 11:43:54 +0530

    RDKB-4878: User is not displayed with any relevant information
    Reason for change: User is not displayed with any relevant information - on saving the current configurations from the 'At the glance' page.
    Test Procedure: Test from GUI.
    Risks: None
    Signed-off-by: Pavan Kumar PavanKumarReddy_B@infosys.com
    
    Change-Id: I53752d0d3e3d66fde3e843365551d5d4ed3fa626

---
+++ source/Styles/xb3/code/trigger_download.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+if (!isset($_SESSION["loginuser"])) {
+	echo '<script type="text/javascript">alert("Please Login First!"); location.href="index.php";</script>';
+	exit(0);
+}
+	$filename="backup.cfg";
+	$filesize=filesize("/var/tmp/".$filename);
+	header ("Content-Type: application/download"); 
+	//header ("Content-Disposition: attachment; filename=backup_".date("YmdHis").".cfg");
+	header ("Content-Disposition: attachment; filename=backup_latest.cfg"); 
+	header("Content-Length: ".$filesize); 
+	$fp=fopen("/var/tmp/".$filename,"r");
+	$buffersize=1024*1024;
+	$curpos=0;
+	while(!feof($fp) && $filesize-$curpos>$buffersize){
+		$buffer=fread($fp,$buffersize);
+		echo $buffer;
+		$curpos+=$buffersize;
+	}
+	$buffer=fread($fp,$filesize-$curpos);
+	echo $buffer;
+	fclose($fp);
+?>
