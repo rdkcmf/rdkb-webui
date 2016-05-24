37006a387027842f9c108747712124b6ade9fdb4
Pavan Kumar <PavanKumarReddy_B@infosys.com>
2016-05-24 15:34:37 +0530

    RDKB-5786: Develop Code - GUI
    Reason for change: Develop GUI Code for VQM logs clear line stats / clear all stats
    Test Procedure: Test from GUI,
    Login as MSO and go to  VQM logs GUI page,
    Clear Line Stats should clear logs based on line 1 OR line 2.
    Clear All Stats should clear logs for both the lines.
    Risks: None
    Signed-off-by: Pavan Kumar PavanKumarReddy_B@infosys.com
    
    Change-Id: I3fb9ff6df440502cbda35cc5f298c6712dd0c0d1

---
+++ source/Styles/xb3/code/actionHandler/ajaxSet_voice_quality_metrics.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ If not stated otherwise in this file or this component's Licenses.txt file the
+ following copyright and licenses apply:
+ Copyright 2016 RDK Management
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+?>
+<?php 
+session_start();
+if (!isset($_SESSION["loginuser"]) || $_SESSION['loginuser'] == 'admin') {
+	echo '<script type="text/javascript">alert("Please Login First!"); location.href="../index.php";</script>';
+	exit(0);
+}
+//data:{line:$("#line_number").val(),call:$("#call_number").val(),action:$("#action").val()},
+$line = $_REQUEST['line'];
+$call = $_REQUEST['call'];
+$action = $_REQUEST['action'];
+if("display" == $action){
+	//display part is handled in main page
+}
+if("clear_line" == $action){
+	setStr("Device.X_CISCO_COM_MTA.ClearLineStats",$line,true);
+}
+else if ("clear_all" == $action){
+	setStr("Device.X_CISCO_COM_MTA.ClearLineStats",1,true);
+	setStr("Device.X_CISCO_COM_MTA.ClearLineStats",2,true);
+}
+?>
