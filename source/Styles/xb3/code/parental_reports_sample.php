598f3a65818dfb60a59d38f00068f9ec61de4d28
jjang <Jiun-Li_Jang@cable.comcast.com>
2016-08-24 18:11:16 +0000

    ARRISXB6-1735 : WEBUI download "HTTP ERROR 500"
    Reason for change: allow report file to be downloaded
    Test Procedure: run WEBUI->parental control->report
    Risks: none
    
    Change-Id: Ic43e3c38298b1f32624c0a3f8c6bb1d442bfb0fd
    Signed-off-by: jjang <Jiun-Li_Jang@cable.comcast.com>

--- source/Styles/xb3/code/parental_reports_sample.php
+++ source/Styles/xb3/code/parental_reports_sample.php
@@ -58,7 +58,7 @@ function file_download($file_path, $file_name)
 set_time_limit(0);
 $report=$_POST['report_type']."_".$_POST['time_frame'];
 $file_path="/var/tmp/parental_reports_".$report.".txt";
-$file_name="parental_repotts_".$report.".txt";
-output_file($file_path, $file_name);
+$file_name="parental_reports_".$report.".txt";
+file_download($file_path, $file_name);
 //output_file("parental_reports_sample.txt","parental_reports_sample.txt");
 ?>
