23fc209dbea1623bb91c6e5f550a0bf89e3eaa52
Pavan Kumar <PavanKumarReddy_B@infosys.com>
2016-10-19 10:11:45 +0530

    RDKB-8565: check & uncheck "Prefer Private Connection" not working
    Reason for change: [Nightly] Alert message "Error! Please try later!" is getting displayed after check & uncheck "Prefer Private Connection"
    Test Procedure: Test from GUI, for mso login - under connected device page add "Prefer private connection" check box
    Risks: None
    Signed-off-by: Pavan Kumar PavanKumarReddy_B@infosys.com
    
    Change-Id: I8284143ec3370111431a4a27dd1d6dbc45b7c334

--- source/Styles/xb3/code/actionHandler/ajaxSet_wireless_network_configuration.php
+++ source/Styles/xb3/code/actionHandler/ajaxSet_wireless_network_configuration.php
@@ -47,7 +47,7 @@ if (array_key_exists('band_steering', $arConfig))
 		}
 	}
 }
-else if (array_key_exists('prefer_private', $arConfig) {
+else if (array_key_exists('prefer_private', $arConfig)) {
 	if ($_SESSION["loginuser"] == "mso") {
 		setStr("Device.WiFi.X_RDKCENTRAL-COM_PreferPrivate", $arConfig['isChecked'], true);
 		echo $arConfig['isChecked'];
