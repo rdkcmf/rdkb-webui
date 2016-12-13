7cf6219131d613d71e40471e4638575868d40b36
Pavan Kumar <PavanKumarReddy_B@infosys.com>
2016-12-13 12:19:34 +0530

    RDKB-9483: prevent XSS in Gateway > Connection > MoCA
    Reason for change: Validation of the input parameter(s) to prevent WebUI - Cross-Site Scripting in Gateway > Connection > MoCA
    Validation of the input parameter(s) is implemented on back-end of GUI page in "actionHandler" folder.
    Test Procedure: Please test and see if GUI is working as expected for Parental Control sub menu pages.
    Use the "Burp Suite" to intercept the requests mentioned below to modify and check, modified invalid requests shouldn't take effect on the XB-3 device.
    Risks: None
    
    Change-Id: I3783a2843604a609f7068a1cbf554fc4e89c99d1
    Signed-off-by: Pavan Kumar PavanKumarReddy_B@infosys.com

--- source/Styles/xb3/code/actionHandler/ajaxSet_moca_config.php
+++ source/Styles/xb3/code/actionHandler/ajaxSet_moca_config.php
@@ -14,6 +14,7 @@
  limitations under the License.
 */
 ?>
+<?php include('../includes/actionHandlerUtility.php') ?>
 <?php
 session_start();
 if (!isset($_SESSION["loginuser"])) {
@@ -25,30 +26,44 @@ $jsConfig = $_POST['configInfo'];
 $arConfig = json_decode($jsConfig, true);
 //print_r($arConfig);
 $thisUser = $_SESSION["loginuser"];
-if ("true" == $arConfig['moca_enable']){
-	if ("admin" != $thisUser){
-		setStr("Device.MoCA.Interface.1.X_CISCO_COM_ChannelScanning", $arConfig['scan_method'], false);
-		if ("false" == $arConfig['scan_method']){
-			setStr("Device.MoCA.Interface.1.FreqCurrentMaskSetting", $arConfig['channel'], false);
-		}
-		setStr("Device.MoCA.Interface.1.BeaconPowerLimit", $arConfig['beacon_power'], false);
-		// GUI version 3.0 removed Taboo enable option
-		// setStr("Device.MoCA.Interface.1.X_CISCO_COM_EnableTabooBit", $arConfig['taboo_enable'], false);
-		// if ("true" == $arConfig['taboo_enable']){
-			setStr("Device.MoCA.Interface.1.NodeTabooMask", $arConfig['taboo_freq'], false);
-		// }
-		setStr("Device.MoCA.Interface.1.PreferredNC", $arConfig['nc_enable'], false);
-		// GUI version 3.0 removed QoS option
-		// setStr("Device.MoCA.Interface.1.QoS.X_CISCO_COM_Enabled", $arConfig['qos_enable'], false);
-	}
-	// GUI version 3.0 don't allowd home user to set MoCA privacy
-	if ("admin" != $thisUser){
-		if ("true" == $arConfig['privacy_enable']){
-			setStr("Device.MoCA.Interface.1.KeyPassphrase", $arConfig['net_password'], false);
-		}
-		setStr("Device.MoCA.Interface.1.PrivacyEnabledSetting", $arConfig['privacy_enable'], false);
-	}
+$validation = true;
+if($validation) $validation = isValInArray($arConfig['moca_enable'], array('true', 'false'));
+if ($validation && "true" == $arConfig['moca_enable'] && "admin" != $thisUser){
+	if($validation) $validation = isValInArray($arConfig['scan_method'], array('true', 'false'));
+	if($validation) $validation = isValInArray($arConfig['channel'], array("0000000000004000", "0000000000008000", "0000000000010000", "0000000000020000", "0000000000040000", "0000000000080000", "0000000000100000", "0000000000200000", "0000000000400000", "0000000000800000", "0000000001000000", "0000000002000000", "0000000004000000", "0000000008000000", "0000000010000000", "0000000020000000", "0000000040000000", "0000000080000000", "0000000100000000", "0000000200000000", ));
+	if($validation) $validation = isValInArray($arConfig['beacon_power'], array('0', '3', '6', '9', '12', '15'));
+	if($validation) $validation = (preg_match("/^0{7}[0-9a-f]{7}00$|^0{16}$/i", $arConfig['taboo_freq'])==1);
+	if($validation) $validation = isValInArray($arConfig['nc_enable'], array('true', 'false'));
+	if($validation) $validation = isValInArray($arConfig['privacy_enable'], array('true', 'false'));
+	if ("true" == $arConfig['privacy_enable'])
+		if($validation) $validation = (preg_match("/^\d{12,17}$/", $arConfig['net_password'])==1);
 }
-setStr("Device.MoCA.Interface.1.Enable", $arConfig['moca_enable'], true);
+if($validation) {
+	if ("true" == $arConfig['moca_enable']){
+		if ("admin" != $thisUser){
+			setStr("Device.MoCA.Interface.1.X_CISCO_COM_ChannelScanning", $arConfig['scan_method'], false);
+			if ("false" == $arConfig['scan_method']){
+				setStr("Device.MoCA.Interface.1.FreqCurrentMaskSetting", $arConfig['channel'], false);
+			}
+			setStr("Device.MoCA.Interface.1.BeaconPowerLimit", $arConfig['beacon_power'], false);
+			// GUI version 3.0 removed Taboo enable option
+			// setStr("Device.MoCA.Interface.1.X_CISCO_COM_EnableTabooBit", $arConfig['taboo_enable'], false);
+			// if ("true" == $arConfig['taboo_enable']){
+				setStr("Device.MoCA.Interface.1.NodeTabooMask", $arConfig['taboo_freq'], false);
+			// }
+			setStr("Device.MoCA.Interface.1.PreferredNC", $arConfig['nc_enable'], false);
+			// GUI version 3.0 removed QoS option
+			// setStr("Device.MoCA.Interface.1.QoS.X_CISCO_COM_Enabled", $arConfig['qos_enable'], false);
+		}
+		// GUI version 3.0 don't allowd home user to set MoCA privacy
+		if ("admin" != $thisUser){
+			if ("true" == $arConfig['privacy_enable']){
+				setStr("Device.MoCA.Interface.1.KeyPassphrase", $arConfig['net_password'], false);
+			}
+			setStr("Device.MoCA.Interface.1.PrivacyEnabledSetting", $arConfig['privacy_enable'], false);
+		}
+	}
+	setStr("Device.MoCA.Interface.1.Enable", $arConfig['moca_enable'], true);
+}
 echo htmlspecialchars($jsConfig, ENT_NOQUOTES, 'UTF-8');
 ?>
