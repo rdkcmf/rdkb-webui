7dd54cd2c3b26154afb299fb9017729184581577
Anoop Nair C <anoopnair_c@infosys.com>
2017-08-24 00:27:52 +0530

    RDKB-13672: User is not redirecting to captive portal due to certificate issue
    Reason for change: After activation user is presented with a security warning due to the https redirection.
    Changes are made to remove the https redirection from lighttpd in case of captive portal.
    
    Test Procedure:
    1) Activate a RDKB device
    2) After activation, unit should be presented with local captive portal without any security warning
    3) Factiry reset/WiFi factory reset, local captive portal is expected without any security warning
    4) While accessing UI after WiFi personalization UI shoudl get redirected to the URL.
    
    Change-Id: I5a0c01774d8101e9e5a7b57b0a79296547f3f08a
    Risks: Low
    Signed-off-by: Anoop Nair Chelakkode<anoopnair_c@infosys.com>

--- source/Styles/xb3/config/webgui.sh
+++ source/Styles/xb3/config/webgui.sh
@@ -54,11 +54,85 @@ source /fss/gw/etc/utopia/service.d/log_env_var.sh
 REVERT_FLAG="/nvram/reverted"
 LIGHTTPD_CONF="/var/lighttpd.conf"
 LIGHTTPD_DEF_CONF="/etc/lighttpd.conf"
+NETWORKRESPONSEVALUE=""
+
+   if [ "$1" = "wan-stopped" ]
+   then
+       if [ "1" != "`sysevent get wanstopped_conf`" ] 
+       then
+          sysevent set wanstopped_conf 1
+       else
+          echo_t "WEBGUI : wan-stopped configuration already in progress: Exit" 
+          exit 1
+       fi
+   fi
+ 
+   if [ "$1" = "wan-started" ] 
+   then
+       if [ "1" != "`sysevent get wanstarted_conf`" ]
+       then
+          sysevent set wanstarted_conf 1
+       else
+          echo_t "WEBGUI : wan-started configuration already in progress: Exit" 
+          exit 1
+       fi
+   fi
+
+configStatus=`sysevent get lighttpdconf`
+if [ "$configStatus" != "inprogress" ]
+then
+   echo_t "WEBGUI :  Set event for configuration in-progress"
+   sysevent set lighttpdconf inprogress
+else
+   itr=0
+   while : ; do
+      configStatus=`sysevent get lighttpdconf`
+      if [ "$configStatus" = "completed" ]
+      then
+          echo_t "WEBGUI : Configuration completed, breaking from loop"
+          break;
+      else
+         itr=$((itr+1))
+         if [ $itr -le 10 ]
+         then
+           echo_t "WEBGUI : Waiting to complete configuration"
+           sleep 3;
+         else
+           echo_t "WEBGUI : Max wait completed, breaking from loop"
+           break;
+         fi
+      fi
+   done
+fi
 
 WEBGUI_INST=`ps | grep webgui.sh | grep -v grep | grep -c webgui.sh`
-if [ $WEBGUI_INST -gt 2 ]; then
-	echo "WEBGUI :Exiting,Another instance running"
-	exit 1
+if [ $WEBGUI_INST -gt 2 ] && [ "$1" = "" ]; then
+  echo "WEBGUI :Exiting,Another instance running"
+  exit 1
+fi
+
+LIGHTTPD_PID=`pidof lighttpd`
+noUrl=0
+
+if [ "$1" = "" ] 
+then
+   wanStatus=`sysevent get wan-status`
+   if [ "$wanStatus" != "started" ]
+   then
+       noUrl=1
+   fi
+fi
+
+# This condition is to handle case where lighttpd will come late
+# after wan. If webgui.sh is called from service_wan during wan-start
+# event and lighttpd hasn't started by that time, then exit
+
+if [ "$1" = "wan-started" ] && [ ! -f "/tmp/webgui_initialized" ]
+then
+    echo_t "WEBGUI : wan started event came, no lighttpd running: exit"
+    sysevent set lighttpdconf completed
+    sysevent set wanstarted_conf 0
+    exit
 fi
 
 LIGHTTPD_PID=`pidof lighttpd`
@@ -67,8 +141,8 @@ if [ "$LIGHTTPD_PID" != "" ]; then
 fi
 
 HTTP_ADMIN_PORT=`syscfg get http_admin_port`
-HTTP_PORT=`syscfg get mgmt_wan_httpport`
-HTTP_PORT_ERT=`syscfg get mgmt_wan_httpport_ert`
+#HTTP_PORT=`syscfg get mgmt_wan_httpport`
+#HTTP_PORT_ERT=`syscfg get mgmt_wan_httpport_ert`
 HTTPS_PORT=`syscfg get mgmt_wan_httpsport`
 BRIDGE_MODE=`syscfg get bridge_mode`
 
@@ -89,46 +163,6 @@ if [ "$HTTP_SECURITY_HEADER_ENABLE" = "true" ]; then
         echo "setenv.add-response-header = (\"X-Frame-Options\" => \"deny\",\"X-XSS-Protection\" => \"1; mode=block\",\"X-Content-Type-Options\" => \"nosniff\",\"Content-Security-Policy\" => \"img-src 'self'; font-src 'self'; form-action 'self';\")"  >> $LIGHTTPD_CONF
 fi
 
-echo "server.port = $HTTP_ADMIN_PORT" >> $LIGHTTPD_CONF
-echo "server.bind = \"$INTERFACE\"" >> $LIGHTTPD_CONF
-echo "\$SERVER[\"socket\"] == \"wan0:80\" { server.use-ipv6 = \"enable\" }" >> $LIGHTTPD_CONF
-
-if [ "x$HTTP_PORT_ERT" != "x" ];then
-    echo "\$SERVER[\"socket\"] == \"erouter0:$HTTP_PORT_ERT\" { server.use-ipv6 = \"enable\" }" >> $LIGHTTPD_CONF
-else
-    echo "\$SERVER[\"socket\"] == \"erouter0:$HTTP_PORT\" { server.use-ipv6 = \"enable\" }" >> $LIGHTTPD_CONF
-fi
-
-echo "\$SERVER[\"socket\"] == \"$INTERFACE:443\" { server.use-ipv6 = \"enable\" ssl.engine = \"enable\" ssl.pemfile = \"/etc/server.pem\" }" >> $LIGHTTPD_CONF
-echo "\$SERVER[\"socket\"] == \"wan0:443\" { server.use-ipv6 = \"enable\" ssl.engine = \"enable\" ssl.pemfile = \"/etc/server.pem\" }" >> $LIGHTTPD_CONF
-if [ $HTTPS_PORT -ne 0 ]
-then
-    echo "\$SERVER[\"socket\"] == \"erouter0:$HTTPS_PORT\" { server.use-ipv6 = \"enable\" ssl.engine = \"enable\" ssl.pemfile = \"/etc/server.pem\" }" >> $LIGHTTPD_CONF
-else
-    # When the httpsport is set to NULL. Always put default value into database.
-    syscfg set mgmt_wan_httpsport 8081
-    syscfg commit
-    HTTPS_PORT=`syscfg get mgmt_wan_httpsport`
-    echo "\$SERVER[\"socket\"] == \"erouter0:$HTTPS_PORT\" { server.use-ipv6 = \"enable\" ssl.engine = \"enable\" ssl.pemfile = \"/etc/server.pem\" }" >> $LIGHTTPD_CONF
-fi
-
-#Changes for ArrisXb6-2949
-
-if [ ! -d "/tmp/pcontrol" ]
-then
-     mkdir /tmp/pcontrol
-fi
-
-cp -rf /usr/www/cmn/ /tmp/pcontrol
-
-if [ ! -f "/tmp/www/index.php" ]
-then
-    cp /usr/www/index_pcontrol.php /tmp/pcontrol/index.php
-fi
-
-echo "\$SERVER[\"socket\"] == \":51515\" { server.use-ipv6 = \"enable\" server.document-root = \"/tmp/pcontrol/\" }" >> $LIGHTTPD_CONF
-
- 
 WIFIUNCONFIGURED=`syscfg get redirection_flag`
 SET_CONFIGURE_FLAG=`psmcli get eRT.com.cisco.spvtg.ccsp.Device.WiFi.NotifyWiFiChanges`
 
@@ -182,9 +216,102 @@ then
 			touch $REVERT_FLAG
 		fi
 	fi
-fi		
+fi	
 
+redirectHttps=true
 
+# "activated" parameter is passed from network_response.sh to indicate
+# that we do not need https redirection as device is put into captive portal
+if [ "$1" = "activated" ]
+then
+    redirectHttps=false
+else
+   isInCP=`dmcli eRT getv Device.DeviceInfo.X_RDKCENTRAL-COM_ConfigureWiFi | grep value | cut -f3 -d: | tr -d ' '`
+   if [ ! -e "$REVERT_FLAG" ] && [ "$WIFIUNCONFIGURED" = "true" ] && [ "$NETWORKRESPONSEVALUE" = "204" ] && [ "$isInCP" = "true" ]
+   then
+       redirectHttps=false
+   fi
+fi
+
+echo "server.port = $HTTP_ADMIN_PORT" >> $LIGHTTPD_CONF
+echo "server.bind = \"$INTERFACE\"" >> $LIGHTTPD_CONF
+
+if [ $noUrl -eq 0 ]
+then
+   if [ "$1" = "wan-stopped" ] 
+   then
+       echo_t "WEBGUI : wan stopped event came, no https redirection"
+       echo -e "\$SERVER[\"socket\"] == \"$INTERFACE:80\" {\n    server.use-ipv6 = \"enable\"\n   \$HTTP[\"host\"] =~ \"(.*)\" {\n        url.redirect = ( \".*\" => \"https://%0:443\$0\" )\n    }\n    }" >> /var/lighttpd.conf
+   else
+       if [ "$redirectHttps" = "false" ]
+       then
+            echo -e "\$SERVER[\"socket\"] == \"$INTERFACE:80\" {\n    server.use-ipv6 = \"enable\"\n     }" >> /var/lighttpd.conf
+       else
+            echo -e "\$SERVER[\"socket\"] == \"$INTERFACE:80\" {\n    server.use-ipv6 = \"enable\"\n    \$HTTP[\"host\"] =~ \"10.0.0.1\" {\n        url.redirect = ( \".*\" => \"https://webui-xb3-cpe-srvr.xcal.tv/\$1\" )\n    }\n    else \$HTTP[\"host\"] =~ \"(.*)\" {\n        url.redirect = ( \".*\" => \"https://%0:443\$0\" )\n    }\n}" >> /var/lighttpd.conf
+       fi
+   fi
+else
+      echo "WEBGUI : No https redirection"
+      echo -e "\$SERVER[\"socket\"] == \"$INTERFACE:80\" {\n    server.use-ipv6 = \"enable\"\n    \$HTTP[\"host\"] =~ \"(.*)\" {\n        url.redirect = ( \".*\" => \"https://%0:443\$0\" )\n    }\n     }" >> /var/lighttpd.conf      
+fi
+
+echo -e "\$SERVER[\"socket\"] == \"wan0:80\" { server.use-ipv6 = \"enable\"\n  \$HTTP[\"host\"] =~ \"(.*)\" {\n    url.redirect = ( \"^/(.*)\" => \"https://%1:443/\$1\" )\n  }\n}" >> /var/lighttpd.conf
+
+#if [ "x$HTTP_PORT_ERT" != "x" ];then
+#    echo "\$SERVER[\"socket\"] == \"erouter0:$HTTP_PORT_ERT\" { server.use-ipv6 = \"enable\" }" >> /var/lighttpd.conf
+#else
+#    echo "\$SERVER[\"socket\"] == \"erouter0:$HTTP_PORT\" { server.use-ipv6 = \"enable\" }" >> /var/lighttpd.conf
+#fi
+
+if [ "$INTERFACE" = "brlan0" ]; then
+    if [ "$1" = "wan-stopped" ] || [ $noUrl -eq 1 ]
+    then
+        echo -e "\$SERVER[\"socket\"] == \"brlan0:443\" {\n    server.use-ipv6 = \"enable\"\n    ssl.engine = \"enable\"\n    ssl.pemfile = \"/tmp/.webui/rdkb-webui.pem\"\n    ssl.ca-file = \"/tmp/.webui/webui-ca.interm.cer\"\n       }" >> /var/lighttpd.conf
+    else
+       if [ "$redirectHttps" = "false" ]
+       then
+          echo -e "\$SERVER[\"socket\"] == \"brlan0:443\" {\n    server.use-ipv6 = \"enable\"\n    ssl.engine = \"enable\"\n    ssl.pemfile = \"/tmp/.webui/rdkb-webui.pem\"\n    ssl.ca-file = \"/tmp/.webui/webui-ca.interm.cer\"\n    }" >> /var/lighttpd.conf
+       else 
+          echo -e "\$SERVER[\"socket\"] == \"brlan0:443\" {\n    server.use-ipv6 = \"enable\"\n    ssl.engine = \"enable\"\n    ssl.pemfile = \"/tmp/.webui/rdkb-webui.pem\"\n    ssl.ca-file = \"/tmp/.webui/webui-ca.interm.cer\"\n    \$HTTP[\"host\"] =~ \"10.0.0.1\" {\n    url.redirect = (\".*\" => \"https://webui-xb3-cpe-srvr.xcal.tv/\$1\")\n }\n}" >> /var/lighttpd.conf
+       fi
+    fi
+else
+    if [ "$1" = "wan-stopped" ] || [ $noUrl -eq 1 ]
+    then
+        echo -e "\$SERVER[\"socket\"] == \"$INTERFACE:443\" {\n    server.use-ipv6 = \"enable\"\n    ssl.engine = \"enable\"\n    ssl.pemfile = \"/tmp/.webui/rdkb-webui.pem\"\n    ssl.ca-file = \"/tmp/.webui/webui-ca.interm.cer\"\n    }" >> /var/lighttpd.conf
+    else
+        echo -e "\$SERVER[\"socket\"] == \"$INTERFACE:443\" {\n    server.use-ipv6 = \"enable\"\n    ssl.engine = \"enable\"\n    ssl.pemfile = \"/tmp/.webui/rdkb-webui.pem\"\n    ssl.ca-file = \"/tmp/.webui/webui-ca.interm.cer\"\n    \$HTTP[\"host\"] !~ \"webui-xb3-cpe-srvr.xcal.tv\" {\n    url.redirect = (\".*\" => \"https://webui-xb3-cpe-srvr.xcal.tv/\$1\")\n }\n}" >> /var/lighttpd.conf
+    fi
+fi
+
+echo "\$SERVER[\"socket\"] == \"wan0:443\" { server.use-ipv6 = \"enable\" ssl.engine = \"enable\" ssl.pemfile = \"/tmp/.webui/rdkb-webui.pem\" ssl.ca-file = \"/tmp/.webui/webui-ca.interm.cer\" }" >> /var/lighttpd.conf
+if [ $HTTPS_PORT -ne 0 ]
+then
+    echo "\$SERVER[\"socket\"] == \"erouter0:$HTTPS_PORT\" { server.use-ipv6 = \"enable\" ssl.engine = \"enable\" ssl.pemfile = \"/tmp/.webui/rdkb-webui.pem\" ssl.ca-file = \"/tmp/.webui/webui-ca.interm.cer\" }" >> /var/lighttpd.conf
+else
+    # When the httpsport is set to NULL. Always put default value into database.
+    syscfg set mgmt_wan_httpsport 8081
+    syscfg commit
+    HTTPS_PORT=`syscfg get mgmt_wan_httpsport`
+    echo "\$SERVER[\"socket\"] == \"erouter0:$HTTPS_PORT\" { server.use-ipv6 = \"enable\" ssl.engine = \"enable\" ssl.pemfile = \"/tmp/.webui/rdkb-webui.pem\" ssl.ca-file = \"/tmp/.webui/webui-ca.interm.cer\" }" >> /var/lighttpd.conf
+fi
+
+#Changes for ArrisXb6-2949
+
+if [ ! -d "/tmp/pcontrol" ]
+then
+     mkdir /tmp/pcontrol
+fi
+
+cp -rf /usr/www/cmn/ /tmp/pcontrol
+
+if [ ! -f "/tmp/www/index.php" ]
+then
+    cp /usr/www/index_pcontrol.php /tmp/pcontrol/index.php
+fi
+
+echo "\$SERVER[\"socket\"] == \":51515\" { server.use-ipv6 = \"enable\" server.document-root = \"/tmp/pcontrol/\" }" >> $LIGHTTPD_CONF
+	
 #echo "\$SERVER[\"socket\"] == \"$INTERFACE:10443\" { server.use-ipv6 = \"enable\" ssl.engine = \"enable\" ssl.pemfile = \"/etc/server.pem\" server.document-root = \"/fss/gw/usr/walled_garden/parcon/siteblk\" server.error-handler-404 = \"/index.php\" }" >> /var/lighttpd.conf
 #echo "\$SERVER[\"socket\"] == \"$INTERFACE:18080\" { server.use-ipv6 = \"enable\"  server.document-root = \"/fss/gw/usr/walled_garden/parcon/siteblk\" server.error-handler-404 = \"/index.php\" }" >> /var/lighttpd.conf
 
@@ -195,8 +322,39 @@ then
 	sed -i "s|${LOG_PATH_OLD}|${LOG_PATH}|g" $LIGHTTPD_CONF
 fi
 
+CONFIGPARAMGEN=/usr/bin/cpgc
+if [ -d /etc/webui/certs ]; then
+       mkdir -p /tmp/.webui/
+       cp /etc/webui/certs/webui-ca.cert.cer /tmp/.webui/
+       cp /etc/webui/certs/webui-ca.interm.cer /tmp/.webui/
+
+       if [ -f $CONFIGPARAMGEN ]; then
+               $CONFIGPARAMGEN jx /etc/webui/certs/nwcwtynbb.ehm /tmp/.webui/rdkb-webui.pem
+               if [ -f /tmp/.webui/rdkb-webui.pem ]; then
+                       chmod 600 /tmp/.webui/rdkb-webui.pem
+               fi
+       else
+               echo "$CONFIGPARAMGEN not found !!!"
+               exit 0
+       fi
+fi
+
 LD_LIBRARY_PATH=/fss/gw/usr/ccsp:$LD_LIBRARY_PATH lighttpd -f $LIGHTTPD_CONF
 
-echo "WEBGUI : Set event"
+if [ -f /tmp/.webui/rdkb-webui.pem ]; then
+       rm -rf /tmp/.webui/rdkb-webui.pem
+fi
+
+echo "WEBGUI : Set events"
+sysevent set lighttpdconf completed
 sysevent set webserver started
+if [ "$1" = "wan-started" ]
+then
+   sysevent set wanstarted_conf 0
+fi
+if [ "$1" = "wan-stopped" ]
+then
+   sysevent set wanstopped_conf 0
+fi
 touch /tmp/webgui_initialized
+
