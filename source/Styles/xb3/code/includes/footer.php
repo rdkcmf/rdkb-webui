54916fd364aea04c3d880efbfdd67f709239e64e
Pavan Kumar <PavanKumarReddy_B@infosys.com>
2018-02-27 10:56:32 +0530

    ARRISXB6-6927, ARRISXB6-6925: footer links Syndication changes
    Reason for change: footer links Syndication changes
    ARRISXB6-6927 footer link texts are updated for respective partner id
    ARRISXB6-6925 footer links mapping is corrected
    Test Procedure:  Test in WebUI,
    footer links should work as expected for Syndication partners
    Risks: None
    
    Change-Id: I43c3f67e001242262cf27b61533a25b6aee9d384
    Signed-off-by: Pavan Kumar PavanKumarReddy_B@infosys.com

--- source/Styles/xb3/code/includes/footer.php
+++ source/Styles/xb3/code/includes/footer.php
@@ -4,16 +4,19 @@
 <?php
 	$footer_param = array(
 		"Device.DeviceInfo.X_RDKCENTRAL-COM_Syndication.RDKB_UIBranding.Footer.PartnerLink",
-		"Device.DeviceInfo.X_RDKCENTRAL-COM_Syndication.RDKB_UIBranding.Footer.UserGuideLink",
 		"Device.DeviceInfo.X_RDKCENTRAL-COM_Syndication.RDKB_UIBranding.Footer.CustomerCentralLink",
+		"Device.DeviceInfo.X_RDKCENTRAL-COM_Syndication.RDKB_UIBranding.Footer.UserGuideLink",
+		"Device.DeviceInfo.X_RDKCENTRAL-COM_Syndication.RDKB_UIBranding.Footer.PartnerText",
+		"Device.DeviceInfo.X_RDKCENTRAL-COM_Syndication.RDKB_UIBranding.Footer.CustomerCentralText",
+		"Device.DeviceInfo.X_RDKCENTRAL-COM_Syndication.RDKB_UIBranding.Footer.UserGuideText",
 	);
 	$footer_value = DmExtGetStrsWithRootObj("Device.DeviceInfo.X_RDKCENTRAL-COM_Syndication.RDKB_UIBranding.", $footer_param);
 ?>
 		<div id="footer">
 			<ul id="footer-links">
-				<li class="first-child"><a href="<?php echo $footer_value[1][1]; ?>" target="_blank">Xfinity.com</a></li>
-				<li style="list-style:none outside none; margin-left:10px">&#8226;&nbsp;&nbsp;<a href="<?php echo $footer_value[2][1]; ?>" target="_blank">customerCentral</a></li>
-				<li style="list-style:none outside none; margin-left:10px">&#8226;&nbsp;&nbsp;<a href="<?php echo $footer_value[3][1]; ?>" target="_blank">User Guide</a></li>
+				<li class="first-child"><a href="<?php echo $footer_value[1][1]; ?>" target="_blank"><?php echo $footer_value[4][1]; ?></a></li>
+				<li style="list-style:none outside none; margin-left:10px">&#8226;&nbsp;&nbsp;<a href="<?php echo $footer_value[2][1]; ?>" target="_blank"><?php echo $footer_value[5][1]; ?></a></li>
+				<li style="list-style:none outside none; margin-left:10px">&#8226;&nbsp;&nbsp;<a href="<?php echo $footer_value[3][1]; ?>" target="_blank"><?php echo $footer_value[6][1]; ?></a></li>
 			</ul>
 		</div> <!-- end #footer -->
 	</div> <!-- end #container -->
