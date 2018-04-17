b75543e0856889eb80c8402e70fa048cbe971aa4
Pavan Kumar <PavanKumarReddy_B@infosys.com>
2018-04-17 08:18:03 +0530

    RDKB-16675: Remove third-party OSS CSRF-Protector-PHP component
    Reason for change: remove the commented code entirely from csrfprotector_rdkb.php
    Test Procedure: 1. CSRF should work as expected
    2. csrfprotector_rdkb.php should have commented code removed entirely
    Risks: None
    
    Change-Id: Ic98f39ef5da25d835b61957d88aea7ce93b34db5
    Signed-off-by: Pavan Kumar PavanKumarReddy_B@infosys.com

--- source/Styles/xb3/code/CSRF-Protector-PHP/libs/csrf/csrfprotector_rdkb.php
+++ source/Styles/xb3/code/CSRF-Protector-PHP/libs/csrf/csrfprotector_rdkb.php
@@ -2,39 +2,6 @@
 include_once __DIR__ ."/csrfprotector.php";
 class csrfprotector_rdkb extends csrfprotector 
 {
-    public static function logCSRFattack() {
-		/*--
-			// logging functionality is commented out
-			$logFile = fopen(self::$config['logDirectory']
-			."/" .date("m-20y") .".log", "a+");
-			
-			//throw exception if above fopen fails
-			if (!$logFile)
-				throw new logFileWriteError("OWASP CSRFProtector: Unable to write to the log file");	
-
-			//miniature version of the log
-			$log = array();
-			$log['timestamp'] = time();
-			$log['HOST'] = $_SERVER['HTTP_HOST'];
-			$log['REQUEST_URI'] = $_SERVER['REQUEST_URI'];
-			$log['requestType'] = self::$requestType;
-
-			if (self::$requestType === "GET")
-				$log['query'] = $_GET;
-			else
-				$log['query'] = $_POST;
-
-			$log['cookie'] = $_COOKIE;
-
-			//convert log array to JSON format to be logged
-			$log = json_encode($log) .PHP_EOL;
-
-			//append log to the file
-			fwrite($logFile, $log);
-
-			//close the file handler
-			fclose($logFile);
-		*/
-    }
+    public static function logCSRFattack() {}
 };
 ?>
