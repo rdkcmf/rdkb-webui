b27e04b92700e87c1ec66d2aa390d266e7df3346
Pavan Kumar <PavanKumarReddy_B@infosys.com>
2018-06-07 15:44:03 +0530

    RDKB-17570: MoCA and WiFi are shown as green in user bar
    Reason for change: MoCA and WiFi are shown as green in user bar in MSO GUI in batterymode
    Test Procedure: In WebGUI,
    In and Out of batterymode, MoCA and WiFi status should be shown as expected
    Risks: None
    
    Change-Id: Ied1457ecbf1148718e7027b64c1a457ec9870979
    Signed-off-by: Pavan Kumar PavanKumarReddy_B@infosys.com

--- source/Styles/xb3/code/actionHandler/ajaxSet_userbar.php
+++ source/Styles/xb3/code/actionHandler/ajaxSet_userbar.php
@@ -43,15 +43,15 @@ if (!isset($_SESSION["loginuser"])) {
 		if($_SESSION['loginuser'] == 'admin') $ssids = array(1,2);
 		foreach ($ssids as $i){
 			$r = (2 - intval($i)%2);	//1,3,5,7==1(2.4G); 2,4,6,8==2(5G)
-			if ("true" == getStr("Device.WiFi.Radio.$r.Enable") && "true" == getStr("Device.WiFi.SSID.$i.Enable")){	//bwg has radio.enable, active status is “at least one SSID and its Radio is enabled”
+			if ("Up" == getStr("Device.WiFi.Radio.$r.Status") && "Up" == getStr("Device.WiFi.SSID.$i.Status")){	//bwg has radio.enable, active status is “at least one SSID and its Radio is enabled”
 				$sta_wifi = "true";
 				break;
 			}
 		}	
 	}
 	if("Disabled"==$_SESSION["psmMode"]) {
-		$sta_moca = getStr("Device.MoCA.Interface.1.Enable"); 
-		$sta_moca = ($sta_moca=="") ? "false" : $sta_moca;
+		$sta_moca = getStr("Device.MoCA.Interface.1.Status");
+		$sta_moca = ($sta_moca=="" || $sta_moca=="Down") ? "false" : "true";
 	}
 
 	$sta_dect = getStr("Device.X_CISCO_COM_MTA.Dect.Enable");
@@ -96,7 +96,7 @@ function get_tips($target, $status)
 			}
 		}break;
 		case "sta_moca":{
-			if ("true"==$status && "Up"==getStr("Device.MoCA.Interface.1.Status")){
+			if ("true"==$status ){
 				$tip = 'Status: Connected-'.getStr("Device.MoCA.Interface.1.X_CISCO_COM_NumberOfConnectedClients").' devices connected';
 			}
 			else{
--- source/Styles/xb3/code/at_a_glance.php
+++ source/Styles/xb3/code/at_a_glance.php
@@ -44,13 +44,15 @@ session_start();
 		if($_SESSION['loginuser'] == 'admin') $ssids = array(1,2);
 		foreach ($ssids as $i){
 			$r = (2 - intval($i)%2);	//1,3,5,7==1(2.4G); 2,4,6,8==2(5G)
-			if ("true" == getStr("Device.WiFi.Radio.$r.Enable") && "true" == getStr("Device.WiFi.SSID.$i.Enable")){	//bwg has radio.enable, active status is “at least one SSID and its Radio is enabled”
+			if ("Up" == getStr("Device.WiFi.Radio.$r.Status") && "Up" == getStr("Device.WiFi.SSID.$i.Status")){	//bwg has radio.enable, active status is “at least one SSID and its Radio is enabled”
 				$sta_wifi = "true";
 				break;
 			}
 		}	
 	}
-	if("Disabled"==$_SESSION["psmMode"]) { $sta_moca = getStr("Device.MoCA.Interface.1.Enable"); }
+	if("Disabled"==$_SESSION["psmMode"]) { $sta_moca = getStr("Device.MoCA.Interface.1.Status");
+		$sta_moca = ($sta_moca=="Down") ? "false" : "true";
+	}
 	$sta_dect = getStr("Device.X_CISCO_COM_MTA.Dect.Enable");
 	$sta_fire = getStr("Device.X_CISCO_COM_Security.Firewall.FirewallLevel");
 	$arConfig = array(
