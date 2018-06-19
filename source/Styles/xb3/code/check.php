6cb5b18a3a8f0d9f1afc7e8b4e7e73f6c985a842
Rajesh Kumar <rajesh4x.kumar@intel.com>
2018-06-19 14:39:27 -0700

    RDKB-264: Xfinity UI admin page "Access denied" while connecting by IPV6 address
    Reason for change: check.php brlan0 ipv6 address parsing logic is not supporting latest kernel 4.9.81 ifcofig output format.
    Test Procedure: CPE client should be able to login Xfinity UI on brlan0 IPV6 address
    Risks: None
    
    Change-Id: Id238375a1711fa9f53cdec880ff32d779c1e3102
    Signed-off-by:  Rajesh Kumar <Rajesh4x.Kumar@intel.com>

--- source/Styles/xb3/code/check.php
+++ source/Styles/xb3/code/check.php
@@ -207,7 +207,8 @@ $server_ip		= $_SERVER["SERVER_ADDR"];
 			else if (strpos($v, 'inet6 addr')){
 				$tmp = explode('Scope', $v);
 				$tmp = explode('addr:', $tmp[0]);
-				$tmp = explode('/', $tmp[1]);
+				$tmp = explode('%', $tmp[1]);
+				$tmp = explode('/', $tmp[0]);
 				array_push($lan_ip, trim($tmp[0]));
 			}
 		}
