56280490c670ce66f587f7d8a51abf11f705b730
Asha Ann <AshaAnn_Mathew@comcast.com>
2019-02-28 11:12:12 +0530

    RDKB-21173:Custom firewall not saved upon selecting checkboxes.
    Reason for change: Custom Security firewall setting for IPv6 are not getting saved upon selecting check boxes.
    Test Procedure:  Progress bar should work in all UI pages with the new library.
    Risks: None
    
    Signed-off-by: Asha Ann AshaAnn_Mathew@comcast.com
    (cherry picked from commit 2c38a3396e388df56a72aaf9b3bf6e89e8d98300)
    (cherry picked from commit efe86f9a93fcd8097d3bd5b0ce8a5d23afd36d55)
    
    Change-Id: Ibf4ed8a4cd92d2863265674c832fcb880f8e3a33

--- source/Styles/xb3/code/actionHandler/ajaxSet_firewall_config_v6.php
+++ source/Styles/xb3/code/actionHandler/ajaxSet_firewall_config_v6.php
@@ -41,14 +41,14 @@ if ( $validation && $firewall_config['firewallLevel'] == "Custom" ){
 if($validation) {
 	if ( $firewall_config['firewallLevel'] == "Custom" )
 	{
-		setStr("Device.X_CISCO_COM_Security.Firewall.FilterHTTPV6", $firewall_config['block_http'], false);
-		setStr("Device.X_CISCO_COM_Security.Firewall.FilterHTTPsV6", $firewall_config['block_http'], false);
+		setStr("Device.X_CISCO_COM_Security.Firewall.FilterHTTPV6", $firewall_config['block_http'], true);
+		setStr("Device.X_CISCO_COM_Security.Firewall.FilterHTTPsV6", $firewall_config['block_http'], true);
             if($modelName != "X5001") {
-		setStr("Device.X_CISCO_COM_Security.Firewall.FilterAnonymousInternetRequestsV6", $firewall_config['block_icmp'], false);
+		setStr("Device.X_CISCO_COM_Security.Firewall.FilterAnonymousInternetRequestsV6", $firewall_config['block_icmp'], true);
                  }
-		setStr("Device.X_CISCO_COM_Security.Firewall.FilterMulticastV6", $firewall_config['block_multicast'], false);
-		setStr("Device.X_CISCO_COM_Security.Firewall.FilterP2PV6", $firewall_config['block_peer'], false);
-		setStr("Device.X_CISCO_COM_Security.Firewall.FilterIdentV6", $firewall_config['block_ident'], false);
+		setStr("Device.X_CISCO_COM_Security.Firewall.FilterMulticastV6", $firewall_config['block_multicast'], true);
+		setStr("Device.X_CISCO_COM_Security.Firewall.FilterP2PV6", $firewall_config['block_peer'], true);
+		setStr("Device.X_CISCO_COM_Security.Firewall.FilterIdentV6", $firewall_config['block_ident'], true);
 	}
 	setStr("Device.X_CISCO_COM_Security.Firewall.FirewallLevelV6", $firewall_config['firewallLevel'], true);
 	// sleep(3);
