59b9484f8c1d74d0b14149acdf548e6a1c8f3b97
mmalep358 <MadhuriLakshmi_Malepati@Comcast.com>
2020-05-11 06:14:58 +0000

    RDKB-28811: Captive Portal is looping with xFi page after FR
    Reason for change: looping should not happen and captive portal should load successfully.
    Test Procedure: After FR load 10.0.0.1 page by connecting to default SSID
                    Captive portal page should load successfully even if cloudUI is enabled.
    
    Risks: Low
    Signed-off-by: Madhuri Lakshmi MadhuriLakshmi_Malepati@comcast.com
    (cherry picked from commit e5871a78351134bdfb3abd666e3d13b1d0fb11b3)
    Change-Id: Ifac8efa4a4f47949dfd0caac836be3b74e3d709d

--- source/Styles/xb3/code/captiveportal.php
+++ source/Styles/xb3/code/captiveportal.php
@@ -298,7 +298,6 @@ ili{
 	$CONFIGUREWIFI 			= getStr("Device.DeviceInfo.X_RDKCENTRAL-COM_ConfigureWiFi");
 	$CaptivePortalEnable	= getStr("Device.DeviceInfo.X_RDKCENTRAL-COM_CaptivePortalEnable");
 	$CloudPersonalizationURL= getStr("Device.DeviceInfo.X_RDKCENTRAL-COM_CloudPersonalizationURL");
-	$CloudUIEnable			= getStr("Device.DeviceInfo.X_RDKCENTRAL-COM_CloudUIEnable");
 	if(!(($personalization_value["Support"] == "true") && ($CaptivePortalEnable == "true") && ($CONFIGUREWIFI == "true"))) {
 		header('Location:index.php');
 		exit;
@@ -416,56 +415,15 @@ ili{
 	$MyAccountAppBox .=	'</div>';
 	$defaultssid = getStr("Device.WiFi.SSID.1.X_COMCAST-COM_DefaultSSID");
 	$defaultssid1 = getStr("Device.WiFi.SSID.2.X_COMCAST-COM_DefaultSSID");
-        $IsCloudReachable = getStr("Device.DeviceInfo.X_RDKCENTRAL-COM_IsCloudReachable");
 ?>
 <script type="text/javascript" src="./cmn/js/lib/jquery-1.9.1.js"></script>
 <script>
 $(document).ready(function(){
 	var Defaultssid = "<?php echo $defaultssid;?>";
 	var Defaultssid1 = "<?php echo $defaultssid1;?>";
-	$CloudPersonalizationURL = "<?php echo $CloudPersonalizationURL;?>";
-	$CloudUIEnable = <?php echo $CloudUIEnable;?>;
-        var IsCloudReachable = "<?php echo $IsCloudReachable;?>";
-	function cloudRedirection(cloudReachable){
-		if(cloudReachable){
-			location.href = $CloudPersonalizationURL;
-		}
-		else{
-			$("#redirect_process").hide();
-			$("#set_up").show();
-		}
-	}
-	if($CloudUIEnable){
-           if(IsCloudReachable) {
-              cloudRedirection(true);
-            }
-           else {
-              cloudRedirection(false);
-            }
-        }
-        else {
-           cloudRedirection(false);
-         }
-		/*$.ajax({
-			type: "POST",
-			url: "actionHandler/ajaxSet_wireless_network_configuration_redirection.php",
-			data: { CloudUIEnable: true },
-			success: function (msg, status, jqXHR) {
-				//msg is the response
-				msg = JSON.parse(msg);
-				if(msg[0] == "true") {
-					cloudRedirection(true);
-				}
-				else{
-					cloudRedirection(false);
-				}
-			}
-		});
-	}
-	else {
-		cloudRedirection(false);
-	}*/
-	// logic t0 figure out LAN or WiFi from Connected Devices List
+        $("#redirect_process").hide();
+        $("#set_up").show();
+   // logic t0 figure out LAN or WiFi from Connected Devices List
 	var connectionType	= "<?php echo $connectionType;?>"; //"Ethernet", "WiFi", "none"
 	var goNextName		= false;
 	var goNextPassword	= false;
