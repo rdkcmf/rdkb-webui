f3c2f85a5869ed54a3f20669babef1e01b33d632
dinesh <dinesh_rameshbabu2@comcast.com>
2020-12-03 13:46:15 +0000

    RDKB-32384: ":" is present only for Frequency Band
    "Private Wi-Fi Network" section in the page Gateway > Connection > Wi-Fi
    
    Reason for change: Remove : from Frequency Band header
    
    Test Procedure: see wi-fi connectino page header
    
    Risks: Low
    
    Signed-off-by: Dinesh_Rameshbabu2@comcast.com
    (cherry picked from commit 2fa58ed1165c161d1f4db112d361ea522501cd34)
    Change-Id: Iffa6cb6e56b7ede31aeb0bcc774c8984e78d4f0d

--- source/Styles/xb3/jst/locale/global.js
+++ source/Styles/xb3/jst/locale/global.js
@@ -889,7 +889,7 @@ $("#wifitip9").html($.i18n("<strong>Manually-Added Wireless Devices:</strong> En
 $("[id^='wiremess1']").text($.i18n("Wi-Fi Radio"));
 $("#wiremess2").text($.i18n("Private Wi-Fi Network"));
 $("#private-Name,#other-Name,#public-Name").text($.i18n("Name"));
-$("#private-Frequency").text($.i18n("Frequency Band:"));
+$("#private-Frequency").text($.i18n("Frequency Band"));
 $("#private-Security,#other-Security,#public-Security").text($.i18n("Security Mode"));
 $("#wiremess3").text($.i18n("Other Wi-Fi Network"));
 $("#other-Frequency,#public-Frequency").text($.i18n("Frequency Band"));
